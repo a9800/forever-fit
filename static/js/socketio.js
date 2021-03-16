6e599d5d9d9ff8118a7abfc9b5cbf7e1adf4a0d7
abdelrahman <abood.wael123@gmail.com>
2021-03-16 20:17:25 +0400

    sockitio fix?

--- static/js/socketio.js
+++ static/js/socketio.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    var socket = io.connect('http://'+document.domain+':'+location.port);
+    var socket = io.connect('//'+document.domain+':'+location.port);
 
     let room = document.getElementById('curr_room').innerText;
     
