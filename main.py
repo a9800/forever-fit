6f67955f5a197fb738b3b6e453021971670a1689
abdelrahman <abood.wael123@gmail.com>
2021-03-17 23:50:36 +0400

    added fontawesome?

--- main.py
+++ main.py
@@ -28,19 +28,6 @@ if __name__ == "__main__":
     def create_table():
         db.create_all()
     
-    @app.after_request
-    def add_cors_headers(response):
-        r = request.referrer[:-1]
-        if r in white:
-            response.headers.add('Access-Control-Allow-Origin', r)
-            response.headers.add('Access-Control-Allow-Credentials', 'true')
-            response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-            response.headers.add('Access-Control-Allow-Headers', 'Cache-Control')
-            response.headers.add('Access-Control-Allow-Headers', 'X-Requested-With')
-            response.headers.add('Access-Control-Allow-Headers', 'Authorization')
-            response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
-        return response
-
     @login.user_loader
     def load_user(uname):
         return User.query.get(uname)
