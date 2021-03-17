89034a92382a34d002b65b0a423a3fbbb62dfdfc
abdelrahman <abood.wael123@gmail.com>
2021-03-17 22:44:14 +0400

    added fontawesome

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ login = LoginManager()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///forever-fit.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-socketio = SocketIO(app, cors_allowed_origins=["https://theforever.fit","https://kit.fontawesome.com","http://kit.fontawesome.com"])
+socketio = SocketIO(app, cors_allowed_origins=["https://theforever.fit","https://fontawesome.com","http://fontawesome.com"])
 
 if __name__ == "__main__":  
     from routes import *
