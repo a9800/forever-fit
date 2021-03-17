bd478e98f822485e014d48ea1f8f77de3798b897
abdelrahman <abood.wael123@gmail.com>
2021-03-17 23:40:57 +0400

    added fontawesome?

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_login import current_user, login_user, LoginManager, login_required, UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO, send
+from flask import request
 import os
 
 from werkzeug import debug
@@ -9,10 +10,12 @@ from werkzeug import debug
 app = Flask(__name__)
 login = LoginManager()
 
+white = ["https://kit.fontawesome.com"]
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///forever-fit.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-socketio = SocketIO(app, cors_allowed_origins=["https://theforever.fit","https://kit.fontawesome.com/a076d05399.js"])
+socketio = SocketIO(app, cors_allowed_origins=["https://theforever.fit"])
 
 if __name__ == "__main__":  
     from routes import *
@@ -24,6 +27,19 @@ if __name__ == "__main__":
     @app.before_first_request
     def create_table():
         db.create_all()
+
+    @app.after_request
+    def add_cors_headers(response):
+        r = request.referrer[:-1]
+        if r in white:
+            response.headers.add('Access-Control-Allow-Origin', r)
+            response.headers.add('Access-Control-Allow-Credentials', 'true')
+            response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+            response.headers.add('Access-Control-Allow-Headers', 'Cache-Control')
+            response.headers.add('Access-Control-Allow-Headers', 'X-Requested-With')
+            response.headers.add('Access-Control-Allow-Headers', 'Authorization')
+            response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+        return response
 
     @login.user_loader
     def load_user(uname):
