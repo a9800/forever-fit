b807b87641d71090a37838bbc6e1ac694ce1c477
abdelrahman <abood.wael123@gmail.com>
2021-03-18 12:07:40 +0400

    Added fontawesome?

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ from werkzeug import debug
 app = Flask(__name__)
 login = LoginManager()
 
-white = ["https://fontawesome.com"]
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///forever-fit.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-socketio = SocketIO(app, cors_allowed_origins=["https://theforever.fit"])
+socketio = SocketIO(app, cors_allowed_origins=["https://theforever.fit","https://fontawesome.com","https://cdnjs.cloudflare.com"])
 
 if __name__ == "__main__":  
     from routes import *
