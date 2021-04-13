83b72b0626ee4ac2cc2270a7577bb32f4b3fb13b
abdelrahman <abood.wael123@gmail.com>
2021-04-14 01:41:44 +0400

    collab changes

--- collaborative_recommender_system.py
+++ collaborative_recommender_system.py
@@ -18,16 +18,32 @@ def get_collaborative_recommendations(uname):
 
     ratings['num_ratings'] = pd.DataFrame(df.groupby('Username')['rating'].count())
 
+    print('ratings')
+    print(ratings.head())
+    print('')
+
     matrix = df.pivot_table(index='client',columns=['Username'],values='rating')
+
+    print('matrix')
+    print(matrix.head())
+    print('')
 
     ratings.sort_values('num_ratings',ascending=False).head()
 
     trainer_rating = matrix[uname]
 
+    print('trainer_rating')
+    print(trainer_rating.head())
+    print('')
+
     similar_to_ah82 = matrix.corrwith(trainer_rating)
 
     corr_trainer = pd.DataFrame(similar_to_ah82,columns=['Correlations'])
     corr_trainer.dropna(inplace=True)
+
+    print('corr_trainer')
+    print(corr_trainer.head())
+    print('')
 
     corr_trainer = corr_trainer.join(ratings['num_ratings'])
 
@@ -42,6 +58,7 @@ def get_collaborative_recommendations(uname):
         i += 1
         if i>2:
             break
+
     print(' ')
     print(recommended_trainers)
     print(' ')
